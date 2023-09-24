7aa61131a294e97a0f57c469434a21200b5311e6
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2023-09-24 09:53:02 +0000

    Basic variable declaration

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 7742156;
+let accountEmail = "pratapJS@gmail.com";
+var accountPassword = "123469";
+    accountCity="Hyderabad";
+
+    // accountId = 66456789;
+
+    // const accountId = "Web@gmail.com"
+
+    // can'nt redeclare block-scope variable 'accountId'
+
+    // don't use var keyword beacuse issues in block and function scope.
+    accountEmail = 'Js@gmail.com';
+    accountCity = "Bengluru";
+
+    //
+
+    console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+    console.log(accountId);
